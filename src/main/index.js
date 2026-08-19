0305dd009f42a9c23510bfffbe1a7ca35e8ab4d1
Fatal-IV <fataliv07@gmail.com>
2026-08-19 12:23:59 +0300

    chore: scaffold Electron shell with single-instance lock

---
+++ src/main/index.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow = null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 960,
+    height: 720,
+    minWidth: 720,
+    minHeight: 560,
+    webPreferences: {
+      preload: path.join(__dirname, '../preload/preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+  mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
+}
+
+const gotLock = app.requestSingleInstanceLock();
+if (!gotLock) {
+  app.quit();
+} else {
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.show();
+      mainWindow.focus();
+    }
+  });
+
+  app.whenReady().then(createWindow);
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+  });
+}
+
+module.exports = { createWindow };
